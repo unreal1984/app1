0605b84929932a5eb3ca18f97dc9373ef3b31ea3
arslan <arslan@mail.ru>
2024-05-16 19:16:14 +0300

    after adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,13 +1,19 @@
-from multiprocessing import context
+#from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 # Create your views here.
 def index(request):
+    
+    
+    categories = Categories.objects.all()
 #    return HttpResponse('Home page')
     context = {
         'title': 'Home - Main page',
         'content': 'Магазин мебели HOME',
+        'categories': categories
         
     }
     return render(request, 'main/index.html', context)   
