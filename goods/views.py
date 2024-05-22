c0ac6288e8bb2c7f304fa081a4052aa10b78bedf
arslan <arslan@mail.ru>
2024-05-23 01:34:45 +0300

    slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 import goods
 from goods.models import Products
 
 # Create your views here.
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    
+    if category_slug=='all':
+        goods = Products.objects.all()
+    else:
+        goods =get_object_or_404( Products.objects.filter(category__slug = category_slug))
+
+
+
+#    goods = Products.objects.all()
 
     context = {
         'title': 'Home catalog',
