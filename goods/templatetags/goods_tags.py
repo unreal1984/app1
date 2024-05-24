4ab18a015ab1a84e3111c63654470aae05f659a9
arslan <arslan@mail.ru>
2024-05-24 10:53:41 +0300

    changed ' on "

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -11,8 +11,8 @@ def tag_categories():
 
 
 @register.simple_tag(takes_context=True)
-def change_params(context,**kwargs):
-    query= context['request'].GET.dict()
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
     query.update(kwargs)
     return urlencode(query)
 
