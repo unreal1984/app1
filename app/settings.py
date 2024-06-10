e46a020c1bc24a8ad9a31fb11d998e6920c2ff43
arslan <arslan@mail.ru>
2024-06-10 15:40:53 +0300

    11

--- app/settings.py
+++ app/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER':'home',
+        'PASSWORD':'home',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+
     }
 }
 
